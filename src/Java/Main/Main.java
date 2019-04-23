b242651e6d6d1edb476a789639d7d95bde877e86
Antoine <antoine_moghaddar@hotmail.com>
2019-04-23 16:52:39 +0200

    Week 1 assignments done | Antoine Moghaddar

--- src/Java/Main/Main.java
+++ src/Java/Main/Main.java
@@ -1,8 +1,62 @@
 package Java.Main;
 
-public class Main {
+import Java.Objects.W1A;
 
-    public void run() {
+import java.util.Scanner;
+
+public class Main {
+    W1A w1a;
+    Scanner sc = new Scanner(System.in);
+    int opt;
+
+    private void run() {
+        w1a = new W1A();
+        menu();
+    }
+
+    public void menupicker() {
+        opt = sc.nextInt();
+        while (opt != 9) {
+            if (w1a.currentWord() != null) {
+                switch (opt) {
+                    case 1:
+                        w1a.setWord();
+                        break;
+                    case 2:
+                        w1a.letterByLetter();
+                        w1a.printArray();
+                        break;
+                    case 3:
+                        w1a.reverse();
+                        w1a.printArray();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println("not a valid option :)");
+                        break;
+                }
+            } else w1a.run();
+            menu();
+            opt = sc.nextInt();
+        }
+    }
+
+    private void menu() {
+        System.out.println("____________________________________");
+        System.out.println("\tcurrentword:\t" + w1a.currentWord());
+
+        System.out.println("1\tAdd word");
+        System.out.println("2\treverse word");
+        System.out.println("3\treverse word");
+        System.out.println("4\toccurences character in word");
+        System.out.println("5\tpalindrome word checker\n\n");
+        System.out.println("9\tExit");
+        System.out.println("____________________________________\n");
+        System.out.println("Provide me with the menu-number of your choice");
+        menupicker();
     }
 
     public static void main(String[] args) {
--- src/Java/Objects/W1A.java
+++ src/Java/Objects/W1A.java
@@ -1,4 +1,52 @@
 package Java.Objects;
 
+import Java.Helperclasses.Antoine.Week1.Word;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class W1A {
+    private Scanner sc = new Scanner(System.in);
+    private ArrayList<Character> chars;
+    private String word;
+
+
+    public void setWord() {
+        System.out.println("Please provide me with a corresponding string below");
+        word = sc.nextLine();
+    }
+
+    public String currentWord() {
+        return word;
+    }
+
+
+    public ArrayList<Character> letterByLetter() {
+        Word w = new Word(word);
+        for (int i = 0; i < w.getWord().length(); i++) {
+            chars.add(w.getWord().charAt(i));
+        }
+        return chars;
+    }
+
+    public void reverse() {
+        Collections.reverse(chars);
+    }
+
+    public void run() {
+        chars = new ArrayList<>();
+        System.out.println("Please provide me with a corresponding string below");
+        word = sc.nextLine();
+    }
+
+    public void printArray() {
+        System.out.println();
+        for (Character letter : chars) {
+            System.out.println(letter);
+        }
+    }
+
+
 }
